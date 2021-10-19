62e914b21f4ad00295beb1280658a0e807b6c345
KUMAR Vikas <vikasparihar864@gmail.com>
2021-10-19 11:23:19 +0530

    spring part

---
+++ springboot-backend/src/main/java/net/java/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,27 @@
+package net.java.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import net.java.springboot.model.User;
+import net.java.springboot.repository.UserRepository;
+
+@SpringBootApplication
+public class SpringbootBackendApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBackendApplication.class, args);
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String...args) throws Exception {
+        this.userRepository.save(new User("Ramesh", "Fadatare", "ramesh@gmail.com"));
+        this.userRepository.save(new User("Tom", "Cruise", "tom@gmail.com"));
+        this.userRepository.save(new User("Tony", "Stark", "tony@gmail.com"));
+    }
+}
---
+++ springboot-backend/src/main/java/net/java/springboot/controller/UserController.java
@@ -0,0 +1,26 @@
+package net.java.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.java.springboot.model.User;
+import net.java.springboot.repository.UserRepository;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("api/")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("users")
+    public List < User > getUsers() {
+        return this.userRepository.findAll();
+    }
+}
